b629b77159b28616d32f33da7d92ec2d3b8a2a3d
Moshe Berman <moshberm@gmail.com>
2012-10-12 02:27:28 -0400

    Ohai, it's a Record class.

--- AccountsReceivable/Record.h
+++ AccountsReceivable/Record.h
@@ -9,6 +9,11 @@
 #ifndef AccountsReceivable_Record_h
 #define AccountsReceivable_Record_h
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 //
 //  An enum to support the usage of the Record struct
 //
@@ -19,15 +24,51 @@ enum RecordType {
     };
 
 //
-//  A structure to hold 
+//  A structure to hold the record information.
+//
+//  This is a class so that we can print it out
+//  as necessary using the description() method.
+//
+//  Everything is public for convenience, otherwise
+//  reading from a stream becomes cumbersome.
 //
 
-struct Record {
+class Record {
+    
+public:
+    
     RecordType type;
-    //Used for either the amount per item, or amount paid. In either case, stored in pennies.
-    int cashAmount;
-    char itemName[0];
+    
+    //
+    //  cashAmount is used for either the price
+    //  per unit, or amount paid, depending on the type.
+    //
+    //  (Usually, it's dumb to use anything but pennies and ints
+    //  for money, but it's a HW assignment, not the next great
+    //  american app idea.)
+    //
+    
+    double cashAmount;
+    char itemName[20];
     int itemQuantity;
+    
+    //
+    //  This method logs out a description of the class.
+    //  It doesn't bother with state so you can see the
+    //  values, regardless of others. (For example, not all
+    //  values of type are paired with a valid itemQuantity
+    //  value.)
+    //
+    
+    void description(){
+        
+        cout << "Record:\n---\nType: " << type;
+        cout << "\nAmount: " << setprecision(2) << fixed << cashAmount;
+        cout << "\nItem: " << itemName;
+        cout << "\nQuantity: " << itemQuantity;
+        cout << '\n';
+    }
+    
 };
 
 #endif
