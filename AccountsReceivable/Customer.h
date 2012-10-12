1c2a03e14eae5ab21350721670379659fa7d42fd
Moshe Berman <moshberm@gmail.com>
2012-10-12 02:27:11 -0400

    Expanded customer class to have a description and some other stuff.

--- AccountsReceivable/Customer.h
+++ AccountsReceivable/Customer.h
@@ -6,13 +6,37 @@
 //  Copyright (c) 2012 Moshe Berman. All rights reserved.
 //
 
+
+
 #ifndef AccountsReceivable_Customer_h
 #define AccountsReceivable_Customer_h
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 class Customer {
+public:
     int customerNumber;
     char name[20];
     int balanceDue;
+    
+    //
+    //  This method logs out a description of the class.
+    //  It doesn't bother with state so you can see the
+    //  values, regardless of others. (For example, not all
+    //  values of type are paired with a valid itemQuantity
+    //  value.)
+    //
+    
+    void description(){
+        
+        cout << "Customer:\n---\nCustomer Number: " << customerNumber;
+        cout << "\nName: " << name;
+        cout << "\nBalance: " << setprecision(2) << fixed << balanceDue;
+        cout << '\n';
+    }
 };
 
 
